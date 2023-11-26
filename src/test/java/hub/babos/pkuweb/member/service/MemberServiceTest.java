2eec0c44e8a4a58adcd599ae8f712a2109004db7
Park Geonho <pagh2322@naver.com>
2023-11-26 18:22:50 +0800

    test: MemberService의 테스트 코드 추가

--- src/test/java/hub/babos/pkuweb/member/service/MemberServiceTest.java
+++ src/test/java/hub/babos/pkuweb/member/service/MemberServiceTest.java
@@ -1,27 +1,30 @@
 package hub.babos.pkuweb.member.service;
 
+import hub.babos.pkuweb.auth.service.PasswordManager;
 import hub.babos.pkuweb.member.domain.Member;
 import hub.babos.pkuweb.member.dto.SignupRequestDto;
 import hub.babos.pkuweb.member.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
-@Transactional
 class MemberServiceTest {
 
-    private final MemberService memberService;
-    private final MemberRepository memberRepository;
-
     @Autowired
-    public MemberServiceTest(MemberService memberService, MemberRepository memberRepository) {
-        this.memberService = memberService;
-        this.memberRepository = memberRepository;
-    }
+    private MemberService memberService;
+
+    @MockBean
+    private MemberRepository memberRepository;
+
+    @MockBean
+    private PasswordManager passwordManager;
 
     @Test
     @DisplayName("새로운 사용자 1명 가입")
@@ -32,12 +35,12 @@ class MemberServiceTest {
                 .nickname("nick")
                 .build();
 
-        memberService.signup(requestDto);
+        Member member = requestDto.toEntity();
 
-        Member savedMember = memberRepository.findByNickname(requestDto.getNickname());
+        Mockito.when(memberRepository.save(any()))
+                .thenReturn(member);
 
-        Assertions.assertThat(savedMember.getEmail())
-                .isEqualTo(requestDto.getEmail());
+        Assertions.assertThat(memberService.signup(requestDto)).isEqualTo(member.getId());
     }
 
 }
